f3a2f27768c98a4dc5aa9c47f83269369c74932d
Marius Berger <mberger@evoila.de>
2019-02-28 16:50:53 +0100

    fix(CloudSiteRepository): Disable export of findByHost method and rename referecnes and endpoints to "site(s)".

--- service-registry/src/main/java/de/evoila/osb/service/registry/data/repositories/CloudSiteRepository.java
+++ service-registry/src/main/java/de/evoila/osb/service/registry/data/repositories/CloudSiteRepository.java
@@ -2,8 +2,12 @@ package de.evoila.osb.service.registry.data.repositories;
 
 import de.evoila.osb.service.registry.model.CloudSite;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource(path="sites", collectionResourceRel = "sites" , itemResourceRel = "site")
 public interface CloudSiteRepository extends CrudRepository<CloudSite, String> {
 
+    @RestResource(exported = false)
     public CloudSite findByHost(String host);
 }
