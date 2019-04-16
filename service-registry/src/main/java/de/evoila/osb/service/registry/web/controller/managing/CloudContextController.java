458cf15b356d8eec9bc5aed6f9357d825e3fdb16
Marius Berger <mberger@evoila.de>
2019-04-16 11:05:06 +0200

    feat(CloudContextController): Add Get endpoints that removed by recent custom implementations.

--- service-registry/src/main/java/de/evoila/osb/service/registry/web/controller/managing/CloudContextController.java
+++ service-registry/src/main/java/de/evoila/osb/service/registry/web/controller/managing/CloudContextController.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +24,8 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestController
@@ -46,6 +49,31 @@ public class CloudContextController extends BaseController {
         this.hateoasBuilder = hateoasBuilder;
         this.userDetailsManager = userDetailsManager;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping(path = "cloudContexts")
+    public ResponseEntity<?> getCloudContexts() {
+        log.info("Received cloudContext list request");
+        List<Resource<CloudContext>> cloudContextResources = new LinkedList<>();
+        for (CloudContext cloudContext : contextManager.getAll()) {
+            Resource <CloudContext> resource = hateoasBuilder.buildResource(cloudContext);
+            resource.getContent().setPassword("<redacted>");
+            cloudContextResources.add(resource);
+        }
+        Resources<Resource<CloudContext>> resources = new Resources<Resource<CloudContext>>(cloudContextResources);
+        resources.add(hateoasBuilder.getLinksForCollection(CloudContextController.class, "cloudContexts"));
+        return new ResponseEntity<Resources<Resource<CloudContext>>>(resources, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "cloudContexts/{contextId}")
+    public ResponseEntity<?> getCloudContext(@PathVariable String contextId) throws ResourceNotFoundException {
+        log.info("Received cloud context get request.");
+
+        Optional<CloudContext> context = contextManager.get(contextId);
+        if (!context.isPresent()) throw new ResourceNotFoundException("cloud context");
+        Resource<CloudContext> resource = hateoasBuilder.buildResource(context.get());
+        resource.getContent().setPassword("<redacted");
+        return new ResponseEntity<Resource<CloudContext>>(resource, HttpStatus.OK);
     }
 
     @PostMapping (path = "cloudContexts")
